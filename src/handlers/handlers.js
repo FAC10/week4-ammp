8de418498bfd4fffcbb6c8a9f2505b08ba33dbe9
PiotrBerebecki <petebere@gmail.com>
2017-03-15 23:59:26 +0000

    Match words from the dictionary file with the query string relates #29
    Use regex to match words, relates #29

--- src/handlers/handlers.js
+++ src/handlers/handlers.js
@@ -4,8 +4,9 @@ const handlers = module.exports = {};
 const querystring = require('querystring');
 const url = require('url');
 
-handlers.serveHomepage = function(request, response) {
-  fs.readFile(path.join(__dirname, '../..', '/public/index.html'), (err, file) => {
+
+handlers.serveHomepage = (request, response) => {
+  fs.readFile(path.join(__dirname, '..', '..', 'public', 'index.html'), (err, file) => {
     if (err) console.log(err);
     response.writeHead(200, {
       'content-type': 'text/html'
@@ -14,12 +15,12 @@ handlers.serveHomepage = function(request, response) {
   });
 };
 
-handlers.serveAssets = function(request, response) {
-  // console.log(request.url);
-  fs.readFile(path.join(__dirname, '../../public', request.url), (err, file) => {
+
+handlers.serveAssets = (request, response) => {
+  fs.readFile(path.join(__dirname, '..', '..', 'public', request.url), (err, file) => {
     if (err) console.log(err);
-    var extension = request.url.split('.')[1];
-    var extensionType = {
+    const extension = request.url.split('.')[1];
+    const extensionType = {
       'html': 'text/html',
       'css': 'text/css',
       'js': 'application/javascript',
@@ -34,91 +35,72 @@ handlers.serveAssets = function(request, response) {
 };
 
 
-
-// handlers.serveResult = (request, response) => {
-//   const searchQuery = url.parse(request.url, true).query.search;
-//
-//   fs.readFile(path.join(__dirname, '..', 'words.txt'), 'utf8', (err, file) => {
-//     if (err) console.log('OH GOD !!!error: ', err);
-//
-//     const wordsArr = file.slice(0, -1).split('\n');
-//     const matchingWordArr = wordsArr.filter(function(word) {
-//       const pattern = new RegExp(searchQuery, 'gi');
-//       return pattern.test(word);
-//     });
-//
-//     console.log(matchingWordArr.length);
-//
-//     response.writeHead(200, { 'content-type': 'application/json' });
-//
-//     response.end(JSON.stringify(matchingWordArr.slice(0, 100)));
-//   });
-// };
-//
-//
-//
-//
-
-
-function getResultArray(str, searchQuery) {
-
-  const wordsArr = str.slice(0, -1).split('\n');
-
-
-  const matchingWordArr = wordsArr.filter(function(word) {
-    const pattern = new RegExp(searchQuery, 'gi');
-    return pattern.test(word);
-  });
-
-  return matchingWordArr;
-}
-
-
-
 handlers.serveResult = (request, response) => {
-  const searchQuery = url.parse(request.url, true).query.search;
+  const searchQuery = url.parse(request.url, true).query.q;
+  const searchQuerySanitized = searchQuery.replace(/[^a-z]/gi, '');
 
-  const readableStream = fs.createReadStream(path.join(__dirname, '..', 'words.txt'));
-  readableStream.setEncoding('utf8');
 
-  let data = '';
+  fs.readFile(path.join(__dirname, '..', 'words.txt'), 'utf8', (err, file) => {
+    if (err) console.log('OH GOD !!!error: ', err);
 
-  readableStream.on('data', (chunk) => {
-    // console.log(chunk);
-    const resultArr = getResultArray(chunk, searchQuery);
-    // console.log(resultArr);
+    const t0 = Date.now();
+    // const wordsArr = file.slice(0, -1).split('\n');
+    // const matchingWordArr = wordsArr.filter(function(word) {
+    //   const pattern = new RegExp(searchQuerySanitized, 'i');
+    //   return pattern.test(word);
+    // });
+    const pattern = new RegExp(`\\b.*${searchQuerySanitized}.*\\b`, 'gi');
+    const matchingWordArr = file.match(pattern) || [];
+
+    const t1 = Date.now();
+    // console.log('duration', t1-t0, 'ms');
+    // console.log(matchingWordArr.length, 'elements');
 
     response.writeHead(200, { 'content-type': 'application/json' });
-
-    // console.log('typeof', typeof JSON.stringify(resultArr));
-
-    response.write(JSON.stringify(resultArr));
-
-    // console.log('chunk', chunk.length);
+    response.end(JSON.stringify(matchingWordArr.slice(0, 100)));
   });
-
-  readableStream.on('end', () => {
-    // response.end(JSON.stringify(['a', 'b']));
-    response.end();
-  });
-
-
-
-  // console.log(matchingWordArr.length);
-
-  // response.writeHead(200, { 'content-type': 'application/json' });
-  //
-  // response.end(JSON.stringify(['a', 'b']));
-
 };
 
 
 
 
-handlers.pageNotFound = function(request, response) {
+
+
+// function getResultArray(str, searchQuery) {
+//   const pattern = new RegExp(`\\b.*${searchQuery}.*\\b`, 'gi');
+//   return str.match(pattern) || [];
+// }
+
+// handlers.serveResult = (request, response) => {
+//   const searchQuery = url.parse(request.url, true).query.search;
+//
+//   const t0 = Date.now();
+//   const readableStream = fs.createReadStream(path.join(__dirname, '..', 'words.txt'));
+//   readableStream.setEncoding('utf8');
+//
+//   let matchingWordArr = [];
+//
+//   readableStream.on('data', (chunk) => {
+//     matchingWordArr = [ ...matchingWordArr, ...getResultArray(chunk, searchQuery)];
+//   });
+//
+//   readableStream.on('end', () => {
+//     const t1 = Date.now();
+//     console.log('duration', t1-t0, 'ms');
+//     console.log(matchingWordArr.length, 'elements');
+//     response.writeHead(200, { 'content-type': 'application/json' });
+//     response.end();
+//   });
+// };
+
+
+
+
+
+handlers.pageNotFound = (request, response) => {
   response.writeHead(404, {
     'content-type': 'text/html'
   });
-  response.write('<h1>404 Page Requested Cannot Be Found</h1>');
+  response.write(`<h1>404: The requested URL ${request.url} was not found on this server.</h1>`);
   response.end();
 };
