95dca65824c38171aaa648ae7e0b03f8c396d264
PiotrBerebecki <petebere@gmail.com>
2017-03-16 15:39:32 +0000

    Add tests for handlers relates #36
    Test handler function relates #36

--- src/handlers/handlers.js
+++ src/handlers/handlers.js
@@ -11,9 +11,7 @@ const handlers = {};
 handlers.serveHomepage = (request, response) => {
   fs.readFile(path.join(__dirname, '..', '..', 'public', 'index.html'), (err, file) => {
     if (err) console.log(err);
-    response.writeHead(200, {
-      'content-type': 'text/html'
-    });
+    response.writeHead(200, { 'content-type': 'text/html' });
     response.end(file);
   });
 };
@@ -30,9 +28,7 @@ handlers.serveAssets = (request, response) => {
       'ico': 'image/x-icon'
     };
 
-    response.writeHead(200, {
-      'content-type': extensionType[extension]
-    });
+    response.writeHead(200, { 'content-type': extensionType[extension] });
     response.end(file);
   });
 };
